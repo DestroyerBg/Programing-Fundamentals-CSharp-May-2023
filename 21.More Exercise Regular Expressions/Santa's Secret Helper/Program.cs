ea6eb91e2b82f6fc1ab19d5500cd04a04bb1ec40
DestroyerBg <gabriel-kulev@abv.bg>
2023-09-09 12:51:52 +0300

    All Exercices

---
+++ 21.More Exercise Regular Expressions/Santa's Secret Helper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Santa_s_Secret_Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfChildren = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                StringBuilder decryptedMessage = new StringBuilder();
+                string pattern = @"\@(?<name>[A-Za-z]+)[^\@\-\!\:\>]*\!(?<behaviour>[G|N])\!";
+                for (int j = 0; j < input.Length; j++)
+                {
+                    char currChar = input[j];
+                    int decryptedCharCode = currChar - numberOfChildren;
+                    char decryptedChar = (char)decryptedCharCode;
+                    decryptedMessage.Append(decryptedChar);
+                }
+
+                Match match = Regex.Match(decryptedMessage.ToString(), pattern);
+                string name = match.Groups["name"].Value;
+                string behaviour = match.Groups["behaviour"].Value;
+                if (behaviour == "G")
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
